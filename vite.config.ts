8588a58de431ca64721d805525d6be620d81fbf4
Kenji Saito <ken-yo@mbr.nifty.com>
2026-05-27 08:57:35 +0900

    Bumps node modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
@@ -13,15 +10,15 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    testTransformMode: {
-      web: ['*.jsx', '*.tsx'],
-    },
+    // testTransformMode: {
+    //   web: ['*.jsx', '*.tsx'],
+    // },
     setupFiles: './setupVitest.ts',
-    // solid needs to be inline to work around
-    // a resolution issue in vitest:
-    deps: {
-      inline: [/solid-js/],
-    },
+    // // solid needs to be inline to work around
+    // // a resolution issue in vitest:
+    // deps: {
+    //   inline: [/solid-js/],
+    // },
     // if you have few tests, try commenting one
     // or both out to improve performance:
     // threads: false,
